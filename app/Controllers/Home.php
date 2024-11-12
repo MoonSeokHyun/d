dbb724ced34114d08e04889bbe5c90bc68560bc3
문석현 <munseoghyeon@Mac.wlan>
2024-11-12 22:22:06 +0900

    s

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,9 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        // 인덱스 뷰 페이지를 반환
+        return view('home');
     }
 }
---
+++ app/Views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - Vegan Community</title>
+</head>
+<body>
+    <h1>Welcome to the Vegan Community!</h1>
+    <p>This is the homepage. Join us to connect with others and share your journey.</p>
+</body>
+</html>
